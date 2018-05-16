a444366f3b9c5acdbca0f958477abffe0aa3fa07
Johannes Schöpp <schoepp@rz.uni-frankfurt.de>
2018-05-16 11:12:54 +0200

    Named all parameters

--- src/hooks/Test-Signature.ps1
+++ src/hooks/Test-Signature.ps1
@@ -3,24 +3,24 @@ $RequireSignature = $true # Every powershell scripts needs a signature
 $CheckForSigErrors = $true # Check powershell scripts for signature errors
 $RequireTimestamp = $true # Does the powershell script need a timestamp signature
 
-$rootDirectory = Join-Path (Split-Path -Parent $MyInvocation.MyCommand.Path) "\..\..\" -Resolve
+$rootDirectory = Join-Path -Path (Split-Path -Parent -Path $MyInvocation.MyCommand.Path) -ChildPath "\..\..\" -Resolve
 $PowershellScripts = Get-ChildItem -Path $rootDirectory -Recurse -Filter "*.ps1" -File
 
 $errSwitch = $false
 
 foreach ($psscript in $PowershellScripts) {
-    $sig = Get-AuthenticodeSignature $psscript.FullName
+    $sig = Get-AuthenticodeSignature -FilePath $psscript.FullName
     if (($sig.Status -ne "Valid") -and $RequireSignature) {
         $errSwitch = $true
-        Write-Output "Signature on file: $($psscript.FullName.Replace($rootDirectory, '.\')) is not valid. Status: $($sig.Status)"
+        Write-Output -InputObject "Signature on file: $($psscript.FullName.Replace($rootDirectory, '.\')) is not valid. Status: $($sig.Status)"
     }
     if (-not (($sig.Status -eq "Valid") -xor ($sig.Status -eq "NotSigned")) -and $CheckForSigErrors) {
         $errSwitch = $true
-        Write-Output "Signature on file: $($psscript.FullName.Replace($rootDirectory, '.\')) is signed but faulty. Status: $($sig.Status)"
+        Write-Output -InputObject "Signature on file: $($psscript.FullName.Replace($rootDirectory, '.\')) is signed but faulty. Status: $($sig.Status)"
     }
     if ($sig -and (!$sig.TimeStamperCertificate -and $RequireSignature)) {
         $errSwitch = $true
-        Write-Output "Signature on file: $($psscript.FullName.Replace($rootDirectory, '.\')) has no timestamp certificate."
+        Write-Output -InputObject "Signature on file: $($psscript.FullName.Replace($rootDirectory, '.\')) has no timestamp certificate."
     }
 }
 
