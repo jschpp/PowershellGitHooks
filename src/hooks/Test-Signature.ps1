d571193f0f96f76f07e922afa1260a0dd719cf67
Johannes Schöpp <schoepp@rz.uni-frankfurt.de>
2018-05-16 15:35:01 +0200

    Small fixes
    * Also look for psm1 and psd1 files
    * Corrected switch for timestamp server

--- src/hooks/Test-Signature.ps1
+++ src/hooks/Test-Signature.ps1
@@ -4,7 +4,7 @@ $CheckForSigErrors = $true # Check powershell scripts for signature errors
 $RequireTimestamp = $true # Does the powershell script need a timestamp signature
 
 $rootDirectory = Join-Path -Path (Split-Path -Parent -Path $MyInvocation.MyCommand.Path) -ChildPath "\..\..\" -Resolve
-$PowershellScripts = Get-ChildItem -Path $rootDirectory -Recurse -Filter "*.ps1" -File
+$PowershellScripts = Get-ChildItem -Path $rootDirectory -Recurse -Filter "*.ps*1" -File
 
 $errSwitch = $false
 
@@ -18,7 +18,7 @@ foreach ($psscript in $PowershellScripts) {
         $errSwitch = $true
         Write-Output -InputObject "Signature on file: $($psscript.FullName.Replace($rootDirectory, '.\')) is signed but faulty. Status: $($sig.Status)"
     }
-    if ($sig -and (!$sig.TimeStamperCertificate -and $RequireSignature)) {
+    if ($sig -and (!$sig.TimeStamperCertificate -and $RequireTimestamp)) {
         $errSwitch = $true
         Write-Output -InputObject "Signature on file: $($psscript.FullName.Replace($rootDirectory, '.\')) has no timestamp certificate."
     }
